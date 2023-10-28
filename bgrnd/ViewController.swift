12ba531837757d2e98d4739d05dde70f179347c0
W. Dusch <w.dusch@squips.de>
2023-10-28 17:08:46 +0200

    Initial Commit

---
+++ bgrnd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bgrnd
+//
+//  Created by Wolfgang Dusch on 28.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
